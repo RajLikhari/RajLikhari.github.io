5bb852fe8006136e30249af666ba6e2b5014f34d
Raj <rajslikhari@gmail.com>
2026-08-21 19:18:20 -0500

    Responsiveness updates

--- src/features/content/cards-section/CardsSection.jsx
+++ src/features/content/cards-section/CardsSection.jsx
@@ -4,7 +4,7 @@ function CardsSection({ title = "", description = "", cards = [] }) {
   return (
     <div class="flex flex-col items-center justify-center mt-20">
       <h1 class="uppercase text-center text-white text-xl font-bold mb-3">{title}</h1>
-      <h2 class="text-white text-lg text-center mb-10 w-3/4">{description}</h2>
+      <h2 class="text-white text-lg text-center mb-10 sm:w-8/10">{description}</h2>
       <div class="flex flex-row flex-wrap justify-center gap-5">
         {cards?.map((card, index) => {
           return <DetailedCard key={index} title={card?.title} subtitle={card?.subtitle} footer={card?.footer} description={card?.description} href={card?.href} />;
--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -1,16 +1,11 @@
 function Hero() {
   return (
     <section class="max-h-screen h-screen w-full flex justify-center items-center flex-col">
-      <img
-        class="h-6/10 w-auto object-contain"
-        src="/animated-figure-gray.png"
-      />
+      <img class="h-6/10 w-auto object-contain" src="/animated-figure-gray.png" />
       <div class="mt-10 flex flex-col items-center">
         <h2 class="text-white text-lg">Hi there, I'm</h2>
-        <h1 class="font-bold text-white text-8xl">Raj Likhari</h1>
-        <h2 class="text-lg text-white bg-zinc-800 rounded-md shadow-lg p-5 mt-10">
-          Software Engineer II @ American Express
-        </h2>
+        <h1 class="font-bold text-white text-5xl sm:text-8xl">Raj Likhari</h1>
+        <h2 class="text-lg text-white bg-zinc-800 rounded-md shadow-lg p-5 mt-10">Software Engineer II @ American Express</h2>
         <div class="flex items-center gap-4 mt-4">
           <span class="relative flex h-5 w-5">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
